b64731e793c5001cad86fa97ccfe3d566e13ee97
Griffin Srednick <griffin.srednick@gmail.com>
2019-08-27 18:06:49 -0700

    Following commit test.

--- 8.27.19_simpsons/tidytues_8.27.19.R
+++ 8.27.19_simpsons/tidytues_8.27.19.R
@@ -2,3 +2,6 @@
 
 ## Notes 
 # to make a new folder directly in terminal : "  mkdir 8_27_19  "
+
+# commit is like saving instantaneously commit often; push occasionally 
+# can revert to go back to most recently commited version
